91405c5b9ed3fe0398af1cac096e9d6e09a2316a
Elisabetta <andreini.elisabetta@gmail.com>
2016-09-23 12:35:00 +0200

    Added Overlay Section Portafoglio 1

--- static/js/main.js
+++ static/js/main.js
@@ -110,3 +110,11 @@
   scrollHandler();
 
 })();
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
